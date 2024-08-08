03f9fc0b1fc059f26aae674d474c917af5ba89c4
codinqu <Bianca.McKinley@mscoding.org>
2024-08-08 14:06:04 -0700

    updated and finished
    updated javascript for coin counter and wrapper calculator finished (finished to the point  of what was required and asked.)

--- script.js
+++ script.js
@@ -5,11 +5,60 @@
 //The program will then output the number of coin wrappers needed and the total amount in dollars.
 
 
-//Instructions
-// 1 - Define Variables for coins 
+//Defining multiple arrays.
 
-let coins = ["pennies", "nickles", "dimes", "quaters"];
+let coins = [.01, .05, .10, .25];
 
-// 2 - Write code to calculate the total value of each type of coin in dollars.
-// Calculate the combined total value of all coins.
+let coinName = ["pennies", "nickles", "dimes", "quaters"];
+
+let coinAmount = [300];
+
+let totalValue = [3, 15, 30, 75];
+
+let wrapperAmount = [50, 40, 50, 40];
+
+
+//Calculations 
+//calculating the final value of coins together 
+let finalValue = (totalValue[0] + totalValue[1] + totalValue[2] + totalValue[3]);
+
+console.log(finalValue);
+//answer for final value amount = 123
+
+//End of first calculations
+
+
+//Wrapper Calculations
+//Calc # of wrappers for each coin
+//Divide each coin: coinAmount / wrapperAmount
+// console.log the whole array : (wrapsFilled)
+
+let pennies = (coinAmount[0] / wrapperAmount[0]);
+let nickles = (coinAmount[0] / wrapperAmount[1]);
+let dimes = (coinAmount[0] / wrapperAmount[2]);
+let quaters = (coinAmount[0] / wrapperAmount[3]);
+
+let wrapsFilled = [6, 7.5, 6, 7.5];
+console.log(wrapsFilled);
+//answer: wrapsFilled = [6, 7.5, 6, 7.5]
+
+//total wrappers filled in general 
+let totalWrappers = (wrapsFilled[0] + wrapsFilled[1] + wrapsFilled[2] + wrapsFilled[3]);
+console.log(totalWrappers);
+//answer: 27
+
+//End of wrapper calculations
+
+
+//Modulus
+//let remainCoins =
+//(# % #) coinAmount % wrapperAmount
+//console.log(# % #);
+
+let remainCoins = (300 % 27);
+console.log(remainCoins);
+//answer: 3
+
+//End of modulus calculation.
+
 
