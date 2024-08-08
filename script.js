b985bc7f40fc47edc06e3aeca322c5e20e09b877
codinqu <Bianca.McKinley@mscoding.org>
2024-08-08 11:03:57 -0700

    to be updated
    the basics. to be updated soon.

---
+++ script.js
@@ -0,0 +1,15 @@
+// Coin Sorter and Wrapper Calculator
+
+//Objective = Create a JavaScript program that calcs the total value of coins in US currency.
+//Sort those coins into coin wrappers.
+//The program will then output the number of coin wrappers needed and the total amount in dollars.
+
+
+//Instructions
+// 1 - Define Variables for coins 
+
+let coins = ["pennies", "nickles", "dimes", "quaters"];
+
+// 2 - Write code to calculate the total value of each type of coin in dollars.
+// Calculate the combined total value of all coins.
+
